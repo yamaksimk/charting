29eb599c6c25dceff2a29929f602f864692a291a
Максим Куракин <maksimkurakin@192.168.1.22>
2024-04-07 03:18:04 +0300

    hw

--- Homework.py
+++ Homework.py
@@ -8,7 +8,6 @@ lst = ['robot']  *  10
 lst += ['human']  *  10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
-one_hot_data = pd.get_dummies(data['whoAmI'])
-data = data.join(one_hot_data)
-data = data.drop('whoAmI', axis=1)
+data['human'] = (data['whoAmI'] == 'human').astype(int)
+data['robot'] = (data['whoAmI'] != 'human').astype(int)
 print(data.head())
